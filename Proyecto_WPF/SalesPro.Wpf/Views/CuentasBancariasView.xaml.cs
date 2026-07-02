2e1e7d71a157a00d1bff1ed1101195c032f5fa1f
Alejandro Porras <ale.porras.oroz.0807@gmail.com>
2026-07-01 23:21:13 -0600

    WPF accesibilidad: foco visible reforzado (doble contorno), temas aplicados a ventanas Buscar, texto legible sobre acento en alto contraste, y apertura de ComboBox por teclado

--- Proyecto_WPF/SalesPro.Wpf/Views/CuentasBancariasView.xaml.cs
+++ Proyecto_WPF/SalesPro.Wpf/Views/CuentasBancariasView.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace SalesPro.Wpf.Views;
 
@@ -7,5 +8,15 @@ public partial class CuentasBancariasView : UserControl
     public CuentasBancariasView()
     {
         InitializeComponent();
+    }
+
+    // Accesibilidad: abrir el ComboBox con Enter o Espacio (además del F4/Alt+Abajo nativo).
+    private void ComboBox_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (sender is ComboBox combo && (e.Key == Key.Enter || e.Key == Key.Space))
+        {
+            combo.IsDropDownOpen = !combo.IsDropDownOpen;
+            e.Handled = true;
+        }
     }
 }
